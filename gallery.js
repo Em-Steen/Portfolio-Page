c4ec6291fc4a7601a1dc045183a9e6ae3be42861
Em-Steen <emma.c_steen@me.com>
2022-11-07 22:38:01 +0000

    now only one js button, but doesn't make it invisible again

--- gallery.js
+++ gallery.js
@@ -1,19 +1,19 @@
 //Have a clickable button on each card that opens and closes the description text
 
-//1 create button in html DONE
-//2 hide description text in html DONE, but I could also set this visible in CSS - maybe easier?
-//3 write JS that makes description text visible when button is clicked
-// - create variable for hidden text
-// - create function that makes <p> = visible when button is clicked
-// - link that function to the html
+//need to amend function to make <p> hidden if visible
+//need to refactor so that I have one function for all button class, not for individual buttons
+
 
 //4 refactor
 
-let p = document.getElementById("description-1");
-let button = document.getElementById("button");
+let button1 = document.getElementById("button1");
 
-function showText() {
- //   p = visible in either html or CSS
-}
+function toggleText() {
+    if (document.getElementById("description-1").style.visibility='hidden') {
+        document.getElementById("description-1").style.visibility='visible';
+    } else {
+        document.getElementById("description-1").style.visibility='hidden';
+        }
+    }
 
-button.addEventListener('click', showText);
+button1.addEventListener('click', toggleText);
