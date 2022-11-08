2f045ae9e389220cf90e606d0da67a685024459d
Em-Steen <emma.c_steen@me.com>
2022-11-08 19:58:09 +0000

    all working

--- gallery.js
+++ gallery.js
@@ -1,19 +1,56 @@
 //Have a clickable button on each card that opens and closes the description text
 
-//need to amend function to make <p> hidden if visible
 //need to refactor so that I have one function for all button class, not for individual buttons
 
+//this function may help to refactor to make function apply to all buttons, but currently not working in this state
+//function toggleText(e) {
+    //if (e.target.parentElement.lastChild.style.visibility == 'hidden') {
+       // e.target.parentElement.lastChild.style.visibility='visible';
+        //} else {
+          //e.target.parentElement.lastChild.style.visibility='hidden';
+        //}
+       //}
 
 //4 refactor
 
 let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
 
-function toggleText() {
-    if (document.getElementById("description-1").style.visibility='hidden') {
-        document.getElementById("description-1").style.visibility='visible';
+function toggleText1() {
+  if (document.getElementById("description-1").style.visibility == 'hidden') {
+    document.getElementById("description-1").style.visibility='visible';
     } else {
         document.getElementById("description-1").style.visibility='hidden';
-        }
     }
+   }
 
-button1.addEventListener('click', toggleText);
+   function toggleText2() {
+    if (document.getElementById("description-2").style.visibility == 'hidden') {
+      document.getElementById("description-2").style.visibility='visible';
+      } else {
+          document.getElementById("description-2").style.visibility='hidden';
+      }
+     }
+
+     function toggleText3() {
+        if (document.getElementById("description-3").style.visibility == 'hidden') {
+          document.getElementById("description-3").style.visibility='visible';
+          } else {
+              document.getElementById("description-3").style.visibility='hidden';
+          }
+         }
+
+         function toggleText4() {
+            if (document.getElementById("description-4").style.visibility == 'hidden') {
+              document.getElementById("description-4").style.visibility='visible';
+              } else {
+                  document.getElementById("description-4").style.visibility='hidden';
+              }
+             }
+
+button1.addEventListener('click', toggleText1);
+button2.addEventListener('click', toggleText2);
+button3.addEventListener('click', toggleText3);
+button4.addEventListener('click', toggleText4);
